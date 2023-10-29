90655a63b42948c2945ff86a370c39e18015903b
yile-li <liyile@umich.edu>
2023-10-29 06:02:58 -0700

    update main page

--- scholar-search-frontend/src/App.js
+++ scholar-search-frontend/src/App.js
@@ -3,25 +3,46 @@ import { Button } from 'react-bootstrap';
 import SearchBar from './components/searchbar';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import QandA from './components/Q&A';
 
 
 function App() {
-  const [summary, SetSummary] = useState('');
+  const [summary, setSummary] = useState('HI this is sample summary');
+  const [practiceMode, setPracticeMode] = useState(false);
+  const [query, setQuery] = useState('');
+
+  const handlePracticeClick = () => {
+    setPracticeMode(true);
+  }
 
   return (
     <div className="App">
       <div className="searchbar">
         <h2>Search in Notes</h2>
         <div className="pad"></div>
-        <SearchBar/>
+        <SearchBar query={query} setQuery={setQuery} setPracticeMode={setPracticeMode}/>
         <div className="pad"></div>
-        <div className="practice-button-div">
-          <Button className="practice-button" variant="outline-primary">Practice with this topic</Button>
+        <div className="buttons-div">
+          <Button className="practice-button" 
+                  variant="outline-primary"
+                  onClick={handlePracticeClick}>
+                    Practice with this topic
+          </Button>
           <Button className="search-history-button" variant="outline-primary">Search History</Button>
         </div>
-        <div className='summary'>
-        </div>
+        {summary && !practiceMode && (
+          <div>
+            <div className="pad"></div>
+            <h2>Summary</h2>
+            <p>{summary}</p>
+          </div>
+        )}
       </div>
+      {practiceMode ? (
+        <div className="practice-div">
+          <QandA practiceMode={practiceMode} query={query} />
+        </div>
+      ) : null}
     </div>
   );
 }
