dfd88a8f09891a5b7c1691c40fa1c58b72adb454
yile-li <liyile@umich.edu>
2023-10-29 04:12:54 -0700

    added search history button

--- scholar-search-frontend/src/App.js
+++ scholar-search-frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import SearchBar from './components/searchbar';
 import './App.css';
@@ -6,18 +6,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
-  const mode = "search";
+  const [summary, SetSummary] = useState('');
+
   return (
     <div className="App">
       <div className="searchbar">
-        <h2>Search</h2>
+        <h2>Search in Notes</h2>
         <div className="pad"></div>
-        <SearchBar mode={mode}/>
+        <SearchBar/>
         <div className="pad"></div>
         <div className="practice-button-div">
           <Button className="practice-button" variant="outline-primary">Practice with this topic</Button>
+          <Button className="search-history-button" variant="outline-primary">Search History</Button>
         </div>
-        
+        <div className='summary'>
+        </div>
       </div>
     </div>
   );
